2d2dfcdb47a9381350779af7c10541fe88e9733b
kaghee <kass.agnes@gmail.com>
2017-11-10 15:37:11 +0100

    trying to display logs

--- src/main/java/com/greenfox/kaghee/chatapp/controllers/ChatAppController.java
+++ src/main/java/com/greenfox/kaghee/chatapp/controllers/ChatAppController.java
@@ -1,13 +1,22 @@
 package com.greenfox.kaghee.chatapp.controllers;
 
+import com.greenfox.kaghee.chatapp.models.LogEntry;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ChatAppController {
-
-    @GetMapping(value="/")
-    public String home() {
+    @RequestMapping(value="/")
+    public String home(Model model) {
+        List<LogEntry> list = new ArrayList<>();
+        list.add(new LogEntry("/", "GET", "INFO"));
+        list.add(new LogEntry("/", "GET", "INFO1"));
+        list.add(new LogEntry("/", "GET", "INFO2"));
+        model.addAttribute("list", list);
         return "home";
     }
 }
---
+++ src/main/java/com/greenfox/kaghee/chatapp/models/LogEntry.java
@@ -0,0 +1,86 @@
+package com.greenfox.kaghee.chatapp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Entity
+public class LogEntry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    String path;
+    String method;
+    String dateAndTime;
+    String logLevel;
+    String requestData;
+
+    public LogEntry(String path, String method, String logLevel, String requestData) {
+        this.path = path;
+        this.method = method;
+        this.dateAndTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH-mm-ss.SS"));
+        this.logLevel = logLevel;
+        this.requestData = requestData;
+    }
+
+    public LogEntry(String path, String method, String logLevel) {
+        this.path = path;
+        this.method = method;
+        this.dateAndTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH-mm-ss.SS"));
+        this.logLevel = logLevel;
+        this.requestData = "";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public void setDateAndTime(String dateAndTime) {
+        this.dateAndTime = dateAndTime;
+    }
+
+    public String getLogLevel() {
+        return logLevel;
+    }
+
+    public void setLogLevel(String logLevel) {
+        this.logLevel = logLevel;
+    }
+
+    public String getRequestData() {
+        return requestData;
+    }
+
+    public void setRequestData(String requestData) {
+        this.requestData = requestData;
+    }
+}
